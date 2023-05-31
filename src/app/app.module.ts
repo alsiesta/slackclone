75b4d7c1c303e933fea3e8b5b57d46d8bf6ebeca
alsiesta <as@daslizenzatelier.de>
2023-05-31 17:13:06 +0200

    installed firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'slackclone-a5cb0',
+    appId: '1:948469326821:web:57fe1d8c2525640ce3450c',
+    storageBucket: 'slackclone-a5cb0.appspot.com',
+    apiKey: 'AIzaSyDg6cotZQTDSZ3K1v99SbrEpumNCP4c0D4',
+    authDomain: 'slackclone-a5cb0.firebaseapp.com',
+    messagingSenderId: '948469326821',
+    measurementId: 'G-BQ10LN7JRY',
+  },};
