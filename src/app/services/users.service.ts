1971e654405c5fb55678c72caae0ba2352116017
alsiesta <as@daslizenzatelier.de>
2023-06-04 18:45:09 +0200

    logging out userData from firestore now

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { FirestoreService } from './firestore.service';
 import { AuthService } from './auth.service';
-import { doc, getDoc } from "firebase/firestore";
-
+import { doc, getDoc, onSnapshot, Firestore } from 'firebase/firestore';
+import * as GLOBAL_VARS from 'src/app/shared/globals';
 import { User } from '../models/user.class';
 import { getAuth, onAuthStateChanged } from '@angular/fire/auth';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  user = new User();
   currentUser$: User;
-  currentUserData: any;
+  $currentUserData: any;
 
-  constructor () { }
+  constructor(private firestoreService: FirestoreService) {}
 
   getCurrentUserId() {
     const auth = getAuth();
@@ -23,4 +24,22 @@ export class UsersService {
     });
   }
 
+  getCurrentUserData() {
+    this.$currentUserData = this.firestoreService.getDocData('V7yyPDgZtYepeluK89vlBHOPYIv1');
+
+    this.$currentUserData.subscribe((response: any) => {
+      this.user.birthDate = response.birthDate;
+      this.user.city = response.city;
+      this.user.displayName = response.displayName;
+      this.user.email = response.email;
+      this.user.firstName = response.firstName;
+      this.user.lastName = response.lastName;
+      this.user.password = response.password;
+      this.user.street = response.birthstreetDate;
+      this.user.uid = response.uid;
+      this.user.zipCode = response.zipCod;
+      console.log(this.user.toJSON());
+      
+    });
+  }
 }
