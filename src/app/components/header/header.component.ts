81520f2f7ec976171d1259df0e2d469284ad695b
alsiesta <as@daslizenzatelier.de>
2023-06-06 20:29:01 +0200

    updated auth.service again

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,78 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsersService } from 'src/app/services/users.service';
-import {
-  collection,
-  Firestore,
-  collectionData,
-  getDoc,
-  doc,
-} from '@angular/fire/firestore';
-import { getAuth, onAuthStateChanged } from '@angular/fire/auth';
-import { UserTemplate } from 'src/app/models/usertemplate.class';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+  
+export class HeaderComponent {
   constructor(
-    private firestore: Firestore,
     public authService: AuthService,
     private router: Router,
     public usersService: UsersService
-  ) {}
+  ) { }
+  
   currentUserDisplayName: string | undefined;
-  user = new UserTemplate();
-  currentUserDisplayName2: string | undefined;
 
-  ngOnInit() {
-    this.usersService.getCurrentUserId();
-    console.log('ngOnInit() called');
-    this.usersService.getCurrentUserData().subscribe((user: UserTemplate) => {
-      this.user = user;
-      console.log('this is my user:', this.user);
-    });
-
-    const auth = getAuth();
-    /////////working version
-    auth.onAuthStateChanged((user) => {
-      const usersCollection = collection(this.firestore, 'users');
-      const docRef = doc(usersCollection, user.uid);
-
-      getDoc(docRef).then((docSnapshot) => {
-        if (docSnapshot.exists()) {
-          const userData = docSnapshot.data();
-          this.currentUserDisplayName = userData['displayName'];
-        }
-      });
-    });
-
-    /////////not working version
-    this.usersService.getCurrentUserDisplayName2().then((displayName) => {
-      this.currentUserDisplayName2 = displayName;
-    });
+  ngOnInit () {
+    this.loglocalStorage()
   }
-
+  
   logOut() {
     this.authService.logOut();
     this.router.navigate(['']);
-  }
-
-  logUserId() {
-    this.usersService.logUserId();
   }
 
   logAllUsers() {
     this.usersService.getAllUsers();
   }
 
-  logCurrentUserData() {
-    console.log(this.currentUserDisplayName);
+  loglocalStorage() {
+    this.currentUserDisplayName = this.authService.getCurrentLocalUser();
   }
-  logCurrentUserData2() {
-    console.log(typeof this.usersService.getCurrentUserDisplayName2());
+
+  logFirebaseAuthCredentials () {
+    this.authService.getAuthCredentials()
   }
+
+  
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HotToastService } from '@ngneat/hot-toast';
 import { FirestoreService } from './firestore.service';
-import { UserTemplate } from '../models/usertemplate.class';
 import {
   Auth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
+  updateProfile,
+  User,
+  getAuth,
 } from '@angular/fire/auth';
 
 @Injectable({
@@ -19,27 +21,26 @@ export class AuthService {
     private firestoreService: FirestoreService,
     private auth: Auth,
     private toast: HotToastService
-  ) { }
-  
+  ) {}
+
   loading: boolean = false;
-  user = new UserTemplate();
+  user: User;
   userId;
 
   signUp = async (email: string, password: string, name: any) => {
-    await createUserWithEmailAndPassword(
-      this.auth,
-      email,
-      password
-    ).then((userCredentials) => {
-      this.userId = userCredentials.user.uid;
-      this.firestoreService.addNewUser(
-        userCredentials.user.uid,
-        name,
-        email,
-        password
-      );
-      this.setLocalUser();
-    });
+    await createUserWithEmailAndPassword(this.auth, email, password).then(
+      (userCredentials) => {
+        this.userId = userCredentials.user.uid;
+        this.firestoreService.addNewUser(
+          userCredentials.user.uid,
+          name,
+          email,
+          password
+        );
+        this.setLocalUser();
+        this.updateAuthCredentials(name);
+      }
+    );
     this.toast.info(`Hi ${name}. Your were successfully signed up`);
   };
 
@@ -64,22 +65,64 @@ export class AuthService {
     return userCredentials;
   };
 
-  setLocalUser () {
+  updateAuthCredentials(dName) {
+    console.log('update Profile Name');
+    const auth = getAuth();
+    updateProfile(auth.currentUser, {
+      displayName: dName,
+      photoURL: 'https://example.com/jane-q-user/profile.jpg',
+    })
+      .then(() => {
+        console.log(auth.currentUser);
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+  }
+
+  getAuthCredentials() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user !== null) {
+      // The user object has basic properties such as display name, email, etc.
+      const displayName = user.displayName;
+      const email = user.email;
+      const photoURL = user.photoURL;
+      const emailVerified = user.emailVerified;
+
+      // The user's ID, unique to the Firebase project. Do NOT use
+      // this value to authenticate with your backend server, if
+      // you have one. Use User.getToken() instead.
+      const uid = user.uid;
+      console.log(displayName, email, photoURL,emailVerified, uid);
+    }
+  }
+
+  setLocalUser() {
     onAuthStateChanged(this.auth, (user) => {
       if (user) {
         this.userData = user;
         localStorage.setItem('SlackUser', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('user')!);
+        // JSON.parse(localStorage.getItem('SlackUser')!);
       } else {
         localStorage.setItem('SlackUser', 'null');
-        JSON.parse(localStorage.getItem('SlackUser')!);
+        // JSON.parse(localStorage.getItem('SlackUser')!);
       }
     });
+  }
+
+  getCurrentLocalUser() {
+    const user = JSON.parse(localStorage.getItem('SlackUser')!);
+    if (user) {
+      console.log('Authenticated User in local storage: ', user);
+      return user.displayName;
+    } else {
+      console.log('No user in local storage');
+    }
   }
 
   logOut() {
     localStorage.removeItem('SlackUser');
     return this.auth.signOut();
   }
-
 }
