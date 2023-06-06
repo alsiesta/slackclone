6a25f64780e33b0d415648e1bbce00bbca8ad819
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-06-06 22:17:28 +0200

    chat component styled and added in main

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -11,13 +11,15 @@ export class ChatService {
     {
       user: 'John Doe',
       image: 'assets/img/threads/profile-picture.png',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
       date: '04.06.2023',
     },
     {
       user: 'Maxi Muster',
       image: 'assets/img/threads/profile-picture.png',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
       date: '04.06.2023',
     },
   ];
