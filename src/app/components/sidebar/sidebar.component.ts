537ae2196deb9ee3fba4bbaf6ab8162368394e83
yannikmorjan <yannik.morjan@web.de>
2023-06-08 17:46:17 +0200

    added drop-down

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  channelsAreOpen = true;
+  directmessagesAreOpen = true;
 
+  ngOnInit(): void {
+    
+  }
+
+  toggleDropdown(key) {
+    switch (key) {
+      case 'ch':
+          this.channelsAreOpen = !this.channelsAreOpen;
+        break;
+
+      case 'dm':
+          this.directmessagesAreOpen = !this.directmessagesAreOpen;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
