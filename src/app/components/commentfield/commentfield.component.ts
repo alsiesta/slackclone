a4b63b63484c55870f1ce8711d8fcdaa4d2a16e4
anilorhan -fwfac <orhan_anil@yahoo.de>
2023-06-10 12:04:12 +0200

    Added Html-tag for quill editor and emoji function for test purposes

--- src/app/components/commentfield/commentfield.component.ts
+++ src/app/components/commentfield/commentfield.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import 'quill-emoji/dist/quill-emoji.js';
 
 
 @Component({
@@ -8,5 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentfieldComponent {
   
+  modules = {}
+
+  constructor() {
+    this.modules = {
+      'emoji-shortname': true,
+      'emoji-textarea': false,
+      'emoji-toolbar': true,
+      'toolbar': {
+        container: [
+          ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+          ['blockquote', 'code-block'],
+
+          [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+          [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+          [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+          [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+          [{ 'direction': 'rtl' }],                         // text direction
+
+          [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+          [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+          [{ 'font': [] }],
+          [{ 'align': [] }],
+
+          ['clean'],                                         // remove formatting button
+
+          ['link', 'image', 'video'],                         // link and image, video
+          ['emoji'],
+        ],
+        handlers: { 'emoji': function () { } }
+      }
+    }
+  }
   
 }
