c3f6d79d5375c4b353d841e234c7fb80bdf890dd
anilorhan -fwfac <orhan_anil@yahoo.de>
2023-06-13 06:46:33 +0200

    Defining the functions for the quill text editor

--- src/app/components/commentfield/commentfield.component.ts
+++ src/app/components/commentfield/commentfield.component.ts
@@ -29,22 +29,22 @@ export class CommentfieldComponent implements OnInit {
           ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
           ['blockquote', 'code-block'],
 
-          [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+          // [{ 'header': 1 }, { 'header': 2 }],               // custom button values
           [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-          [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
-          [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
-          [{ 'direction': 'rtl' }],                         // text direction
+          // [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+          // [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+          // [{ 'direction': 'rtl' }],                         // text direction
 
-          [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+          // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+          // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
-          [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-          [{ 'font': [] }],
-          [{ 'align': [] }],
+          // [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+          // [{ 'font': [] }],
+          // [{ 'align': [] }],
 
-          ['clean'],                                         // remove formatting button
+          // ['clean'],                                         // remove formatting button
 
-          ['link', 'image', 'video'],                         // link and image, video
+          // ['link', 'image', 'video'],                         // link and image, video
           ['emoji'],
         ],
         handlers: { 'emoji': function () { } }
--- src/app/components/threads/threads.component.ts
+++ src/app/components/threads/threads.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-threads',
   templateUrl: './threads.component.html',
   styleUrls: ['./threads.component.scss']
 })
-export class ThreadsComponent {
+export class ThreadsComponent implements OnInit {
 
+  constructor(public firestoreService: FirestoreService) {
+    
+  }
+  
+  ngOnInit(): void {
+    
+  }
 }
