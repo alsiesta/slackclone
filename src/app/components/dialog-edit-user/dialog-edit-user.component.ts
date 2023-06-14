753e9103aa77f316e60fb413c8111c230f26aa6a
alsiesta <as@daslizenzatelier.de>
2023-06-14 19:09:08 +0200

    dialog edit user editing currentUser Profile work-in-progress

--- src/app/components/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/components/dialog-edit-user/dialog-edit-user.component.ts
@@ -1,6 +1,11 @@
-import {Component, Inject} from '@angular/core';
-import {MatDialogRef,MatDialog, MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
-import {NgIf} from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import {
+  MatDialogRef,
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { UsersService } from 'src/app/services/users.service';
 import { UserTemplate } from 'src/app/models/usertemplate.class';
@@ -16,27 +21,48 @@ export interface DialogData {
 @Component({
   selector: 'app-dialog-edit-user',
   templateUrl: './dialog-edit-user.component.html',
-  styleUrls: ['./dialog-edit-user.component.scss']
+  styleUrls: ['./dialog-edit-user.component.scss'],
 })
 export class DialogEditUserComponent {
-  constructor (@Inject(MAT_DIALOG_DATA) public data: DialogData, public usersService: UsersService) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    public usersService: UsersService
+  ) {}
 
   currentUserData$: UserTemplate;
+  isEditing = false;
+  currentUserId$: string = this.usersService.currentUserId$
 
-  ngOnInit () {
-   return this.usersService.getCurrentUserData()
-      .then((data) => {
-        this.currentUserData$ = data;
-        console.log(this.currentUserData$);
-      })
-}
+  ngOnInit() {
+    this.getCurrentUserData();
 
 
- currentUserFirstName$() {
-    
+  }
+
+  getCurrentUserData () {
+    return this.usersService.getCurrentUserData().then((data) => {
+      this.currentUserData$ = data;
+      console.log(this.currentUserData$);
+    });
+  }
+  getCurrentUserId () {
+    return this.usersService.getCurrentUserData().then((data) => {
+      this.currentUserData$ = data;
+      this.currentUserId$ = data
+      console.log(this.currentUserData$);
+    });
   }
   
-      
-      
-  
+  updateUserProperty (field,value) {
+    this.usersService.updateUserFieldValue(field,value)
+  }
+
+  cancelEdit() {
+    this.isEditing = false;
+  }
+
+  startEdit() {
+    this.isEditing = true;
+  }
+
 }
