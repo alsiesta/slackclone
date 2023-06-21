86f4acec3b6d04450ded69571295c49f267a787c
alsiesta <as@daslizenzatelier.de>
2023-06-21 23:16:13 +0200

    finished functionality of user-edit

--- src/app/components/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/components/dialog-edit-user/dialog-edit-user.component.ts
@@ -14,11 +14,17 @@ export interface DialogData {
   uid;
 }
 export interface IsEditing {
-  displayName?:boolean;
-  email?:boolean;
-  photoUrl?:boolean;
-  emailVerified?:boolean;
-  uid?:boolean;
+  displayName?: boolean;
+  firstName?: boolean;
+  lastName?: boolean;
+  email?: boolean;
+  photoUrl?: boolean;
+  emailVerified?: boolean;
+  city?: boolean;
+  zipCode?: boolean;
+  street?: boolean;
+  birthDate?: boolean;
+  uid?: boolean;
 }
 
 @Component({
@@ -29,25 +35,41 @@ export interface IsEditing {
 export class DialogEditUserComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    public usersService: UsersService, public firestoreService: FirestoreService,
+    public usersService: UsersService,
+    public firestoreService: FirestoreService
   ) {}
 
   currentUserData$: UserTemplate;
-  isEditing = false;
+
+  isEditing: IsEditing = {
+    displayName: false,
+    firstName: false,
+    lastName: false,
+    email: false,
+    city: false,
+    zipCode: false,
+    street: false,
+    photoUrl: false,
+    birthDate: false,
+    emailVerified: false,
+    uid: false,
+  };
+
   currentUser$: any;
   currentUserId$: string;
   currentUser: UserTemplate;
   counter: number = 0;
   chat: any;
-  
+
   ngOnInit() {
     this.currentUserId$ = this.usersService.getCurrentUserId();
     this.observable.subscribe(this.subscriber);
-    this.firestoreService.observeChat$.subscribe((data) => {console.log(data); this.chat = data;});
-    
+    this.firestoreService.observeChat$.subscribe((data) => {
+      console.log(data);
+      this.chat = data;
+    });
   }
 
-  
   observable = new Observable((subscriber) => {
     // subscriber.next(this.currentUserId$);
     subscriber.next(
@@ -61,8 +83,6 @@ export class DialogEditUserComponent {
     // subscriber.error();
     subscriber.complete();
   });
-
-
 
   subscriber = {
     next: (data) => {
@@ -84,14 +104,84 @@ export class DialogEditUserComponent {
 
   updateUserProperty(field, value) {
     this.usersService.updateUserFieldValue(field, value);
-    this.isEditing = false;    
+    this.cancelEdit(field);
   }
 
-  cancelEdit() {
-    this.isEditing = false;
+  log(displayName) {
+    console.log(displayName);
   }
 
-  startEdit() {
-    this.isEditing = true;
+  cancelEdit(field) {
+    switch (field) {
+      case 'displayName':
+        this.isEditing.displayName = false;
+        break;
+      case 'firstName':
+        this.isEditing.firstName = false;
+        break;
+      case 'lastName':
+        this.isEditing.lastName = false;
+        break;
+      case 'email':
+        this.isEditing.email = false;
+        break;
+      case 'city':
+        this.isEditing.city = false;
+        break;
+      case 'street':
+        this.isEditing.street = false;
+        break;
+      case 'zipCode':
+        this.isEditing.zipCode = false;
+        break;
+      case 'birthDate':
+        this.isEditing.birthDate = false;
+        break;
+      case 'photoUrl':
+        this.isEditing.photoUrl = false;
+        break;
+      case 'emailVerified':
+        this.isEditing.emailVerified = false;
+        break;
+      default:
+        break;
+    }
+  }
+
+  startEdit(field) {
+    switch (field) {
+      case 'displayName':
+        this.isEditing.displayName = true;
+        break;
+      case 'firstName':
+        this.isEditing.firstName = true;
+        break;
+      case 'lastName':
+        this.isEditing.lastName = true;
+        break;
+      case 'email':
+        this.isEditing.email = true;
+        break;
+      case 'city':
+        this.isEditing.city = true;
+        break;
+      case 'street':
+        this.isEditing.street = true;
+        break;
+      case 'zipCode':
+        this.isEditing.zipCode = true;
+        break;
+      case 'birthDate':
+        this.isEditing.birthDate = true;
+        break;
+      case 'photoUrl':
+        this.isEditing.photoUrl = true;
+        break;
+      case 'emailVerified':
+        this.isEditing.emailVerified = true;
+        break;
+      default:
+        break;
+    }
   }
 }
