30002af64370e6303433e7e1e1cc595e5a0a07ad
alsiesta <as@daslizenzatelier.de>
2023-06-26 00:14:50 +0200

    activated authGuard and sign-in protection

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private authService: AuthService) {}
+
+  canActivate() {
+    if (this.authService.isUserLoggedIn) {
+      return true;
+    } else {
+      return false;
+    }
   }
+
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
