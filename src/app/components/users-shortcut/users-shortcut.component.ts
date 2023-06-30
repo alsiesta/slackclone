ab75dff73ca52eba20e4701f27a673b7b5f2cd76
yannikmorjan <yannik.morjan@web.de>
2023-06-30 22:58:11 +0200

    updated user shortcut

--- src/app/components/users-shortcut/users-shortcut.component.ts
+++ src/app/components/users-shortcut/users-shortcut.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChannelService } from 'src/app/services/channel.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-users-shortcut',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./users-shortcut.component.scss']
 })
 export class UsersShortcutComponent {
+  userList: any[];
 
+  constructor(
+    private usersService: UsersService,
+    private channelService: ChannelService,
+  ) {
+    // Retrieve the user list from the usersService and deletes the currentUser from it.
+    this.userList = this.usersService.currentUsers;
+    let currentUser = this.usersService.currentUserId$;
+    this.userList = this.userList.filter(user => user.uid !== currentUser);
+  }
+
+  openDialog(displayName, photoURL, email, uid) {
+    this.channelService.messageDialogOpen(displayName, photoURL, email, uid)
+  }
+  
 }
