c8c6ae7622eabfebc5a327f127a34ec42d65dbfc
yannikmorjan <yannik.morjan@web.de>
2023-06-30 23:08:07 +0200

    updated button direction dialog-new-message

--- src/app/components/dialog-new-message/dialog-new-message.component.ts
+++ src/app/components/dialog-new-message/dialog-new-message.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ChatService } from 'src/app/services/chat.service';
+import { GlobalService } from 'src/app/services/global.service';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -15,6 +16,7 @@ export class DialogNewMessageComponent {
     public data: { user: string; image: string; email: string; id: string },
     public chatService: ChatService,
     public dialogRef: MatDialogRef<DialogNewMessageComponent>,
-    public userSevice: UsersService
+    public userSevice: UsersService,
+    public globalService: GlobalService,
   ) {}
 }
